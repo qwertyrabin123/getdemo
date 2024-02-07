fbf96c309c2987eb3bd0427c6a43a83f083f2d1e
Rabin Awale <hellorabin123awale@gmail.com>
2024-02-07 12:32:59 +0545

    some of git tool kits

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<h1>Final Version</h1>
+some term used in git
+
+//git init: initiazlize the project
+
+// add specific or all
+git add . to add all the file
+
+git add "*.js" only add the js
+git add "*.php" only add the php
+
+//shortent like 
+instead of git status we can use only git s 
+for that we have to configure to configure
+git config --global alias.s "status" 
+here alias->s will now work as
+git s instead of status
+
+
+git config --global alias.cm "commit -m"
+shorten for git commit -m
+
+
+
+git log -> to log the version
+git log --all ->more detail log
+
